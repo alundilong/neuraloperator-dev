e348800e65034ba04932776ab6ce5f84843ae5df
alundilong <ymao.mu@gmail.com>
2025-03-14 15:52:38 -0500

    feat:disable mask tensor

--- melting/train_melting.py
+++ melting/train_melting.py
@@ -151,7 +151,8 @@ else:
 
 
 # Creating the losses
-l2loss = LpLoss(d=3, p=2, data_processor = data_processor,loss_type=config.opt.loss_type, mask_channel_outputs=[0,2,3], mask_channel=5)
+#l2loss = LpLoss(d=3, p=2, data_processor = data_processor,loss_type=config.opt.loss_type, mask_channel_outputs=[0,2,3], mask_channel=5)
+l2loss = LpLoss(d=3, p=2, data_processor=data_processor,loss_type=config.opt.loss_type, mask_channel_outputs=None, mask_channel=5)
 
 if config.opt.training_loss == "l2":
     train_loss = l2loss
